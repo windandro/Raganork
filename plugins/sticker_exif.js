bd46047b817ee9aebcad71c36d1a02f263ef5a66
byadems <81581944+byadems@users.noreply.github.com>
2022-02-12 20:24:21 +0300

    Update sticker_exif.js

--- plugins/sticker_exif.js
+++ plugins/sticker_exif.js
@@ -1,7 +1,7 @@
 /* (c) souravkl11/raganork
 You may not use this file except compliance with license!*/
 let e = require('../events');
-let { MessageType } = require('@adiwajshing/baileys');
+let { MessageType, Mimetype } = require('@adiwajshing/baileys');
 let w = require('../config');
 let v = w.SUPPORT3
 let i = require('raganork-bot');
@@ -10,7 +10,7 @@ let ffmpeg = require('fluent-ffmpeg');
 const h = require('heroku-client');
 const he = new h({token: w.HEROKU.API_KEY});
 let ur = '/apps/' + w.HEROKU.APP_NAME;
-e.addCommand({pattern: 'take ?(.*)', fromMe: a, desc:'Changes sticker pack & author name!'}, (async (m, match) => { 
+e.addCommand({pattern: 'take ?(.*)', fromMe: a, desc:'Changes sticker/audio pack & author name. Title, artist, thumbnail etc.'}, (async (m, match) => { 
 if (!m.reply_message.data.quotedMessage) return await m.sendMessage('_Reply to an audio or a sticker_')
 var audiomsg = m.reply_message.data.quotedMessage.audioMessage;
 var stickermsg = m.reply_message.data.quotedMessage.stickerMessage;
@@ -19,14 +19,10 @@ if (stickermsg) {
 var s = w.SOURAVKL11.split('|');
 var au = s[1];
 var p = s[0];
-ffmpeg(q)
-.fromFormat('webp_pipe')
-.save('ex.png')
-.on('end', async () => {
-var res = await i.query.sticker('ex.png',au,p,v)
-await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});})
+var res = await i.query.sticker(q,au,p,v)
+await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});
 } 
-else if (audiomsg) {
+if (!stickermsg && audiomsg) {
 ffmpeg(q)
 .save('info.mp3')
 .on('end', async () => {
@@ -36,7 +32,7 @@ var spl = w.AUDIO_DATA.split(';')
     });
     
 }
-else return await m.sendMessage('_Reply to an audio or a sticker_')
+if (!audiomsg && !stickermsg) return await m.client.sendMessage(m.jid,'_Reply to an audio or a sticker_',MessageType.text,{quoted: m.data})
 }));
 e.addCommand({pattern: 'wm ?(.*)', fromMe: a, desc:'Sets sticker pack & author name with given ones.'}, (async (m, t) => { 
 var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
@@ -47,24 +43,16 @@ au = s[1];
 p = s[0];}
 else {
 p = t[1]}
-ffmpeg(q)
-.fromFormat('webp_pipe')
-.save('ex.png')
-.on('end', async () => {
-var res = await i.query.sticker('ex.png',au,p,v)
-await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});})}));
+var res = await i.query.sticker(q,au,p,v)
+await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});}));
 e.addCommand({pattern: 'crop ?(.*)', fromMe: a, desc:'Crops sticker'}, (async (m, t) => { 
 var q = await m.client.downloadAndSaveMediaMessage({key: {remoteJid: m.reply_message.jid,id: m.reply_message.id},message: m.reply_message.data.quotedMessage});
 var au,p;
 var s = w.SOURAVKL11.split('|');
 var au = s[1];
 var p = s[0];
-ffmpeg(q)
-.fromFormat('webp_pipe')
-.save('ex.png')
-.on('end', async () => {
-var res = await i.query.stickercrop('ex.png',au,p,v)
-await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});})}));
+var res = await i.query.stickercrop(q,au,p,v)
+await m.client.sendMessage(m.jid, await i.query.skbuffer(res),MessageType.sticker,{quoted:m.data});}));
 e.addCommand({ pattern: 'setexif ?(.*)', fromMe: true}, (async (m, qu) => {
 if (!qu[1]) {return await m.client.sendMessage(m.jid,'_Need some data \n Example: \n .setexif Name|Author_',MessageType.text,{quoted:m.data})}
 await m.client.sendMessage(m.jid, '_Added new exif!_',MessageType.text,{quoted:m.data});
