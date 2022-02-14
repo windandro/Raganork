7717e59c0d49c7e1cd65210c690ce2ba4923f7a2
byadems <81581944+byadems@users.noreply.github.com>
2022-02-14 20:24:24 +0300

    Update delete.js

--- plugins/delete.js
+++ plugins/delete.js
@@ -1,8 +1,1 @@
-const New = require('../events');
-const {MessageType} = require('@adiwajshing/baileys');
-const Language = require('../language');
-const Lang = Language.getString('admin');
 
-New.addCommand({pattern: 'del', fromMe: true, desc: Lang.DEL_DESC}, (async (multidevice, match) => {
-await multidevice.client.deleteMessage(multidevice.jid, {id: multidevice.reply_message.id, remoteJid: multidevice.reply_message.jid, fromMe: true})
-}));
