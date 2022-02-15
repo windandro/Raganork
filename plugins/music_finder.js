4c96dd1dbd028334e737eca57b5de4bf89b2532d
byadems <81581944+byadems@users.noreply.github.com>
2022-02-15 21:40:33 +0300

    Update music_finder.js

--- plugins/music_finder.js
+++ plugins/music_finder.js
@@ -4,14 +4,18 @@ const c = require('../config')
 var f = require('raganork-bot')
 var v = c.SESSION
 var fm = c.WORKTYPE == 'public' ? false : true
-e.addCommand({pattern: 'find ?(.*)', fromMe: fm}, (async (m, match) => {    
+
+const Language = require('../language');
+const Lang = Language.getString('finder');
+
+e.addCommand({pattern: 'find ?(.*)', fromMe: fm, desc: Lang.FINDER_DESC}, (async (m, match) => {    
 if (!m.reply_message.text && !m.reply_message.video && !m.reply_message.sticker && !m.reply_message.image) {
 var q = await m.client.downloadAndSaveMediaMessage({key: { remoteJid: m.reply_message.jid,id: m.reply_message.id}, message: m.reply_message.data.quotedMessage});
 var k = c.find_key
 var r = await f.query.music(q,k,v)
 if (r.result) {
-let msg =  '_Title:_ *' + r.result.title + '* \n_Album:_ *' + r.result.album+'* \n_Artist:_ *' + r.result.artist+ '* \n_Label:_ *' + r.result.label+'* \n_Release date:_ *' + r.result.release_date + '* \n_Song link:_ ' + 'https://www.youtube.com/results?search_query='+r.result.title.split(' ').join('+')
+let msg =  Lang.TITLE + r.result.title + Lang.ALBUM + r.result.album + Lang.ARTIST + r.result.artist + Lang.LABEL + r.result.label + Lang.RDATE + r.result.release_date + Lang.SLINK + 'https://www.youtube.com/results?search_query='+r.result.title.split(' ').join('+')
 return await m.client.sendMessage(m.jid, msg, MessageType.text, {quoted: m.data})}
-if (r.error && r.error.error_code == '900') return await m.client.sendMessage(m.jid, "_API Key expired! Get new api key_", MessageType.text,{quoted: m.data})
-if (!r.result) return await m.client.sendMessage(m.jid, "_Not found!_", MessageType.text,{quoted: m.data})}
-else {return await m.client.sendMessage(m.jid, "_Reply to any music!_", MessageType.text,{quoted: m.data})}}));
+if (r.error && r.error.error_code == '900') return await m.client.sendMessage(m.jid, Lang.APIERR, MessageType.text,{quoted: m.data})
+if (!r.result) return await m.client.sendMessage(m.jid, Lang.NO_RESULT, MessageType.text,{quoted: m.data})}
+else {return await m.client.sendMessage(m.jid, Lang.NEED_REPLY, MessageType.text,{quoted: m.data})}}));
