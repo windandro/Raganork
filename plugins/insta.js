db7940abf1b0342c322a971a7e8c1454e08426f9
byadems <81581944+byadems@users.noreply.github.com>
2022-02-28 19:41:45 +0300

    Update insta.js

--- plugins/insta.js
+++ plugins/insta.js
@@ -63,14 +63,18 @@ if (query[1] === '') return await msg.client.sendMessage(msg.jid, Lang.NEED_WORD
 var user = query[1];
 var res = await raganork.query.getStory(user,v)
 if (res === "false") return await msg.client.sendMessage(msg.jid, Lang.NOT_FOUND, MessageType.text, {quoted: msg.data})
+if (res.error) return await msg.client.sendMessage(msg.jid, res.error.replace('status','story'), MessageType.text, {quoted: msg.data})
 var url = ''
 res.result.stories.map((result) => {
 url += result.url + ','});
 var que = url !== false ? url.split(',') : [];
 for (var i = 0; i < (que.length < res.result.stories.length ? que.length : res.result.stories.length); i++) {
 var get = got(que[i], {https: {rejectUnauthorized: false}});
+var type = que[i].includes('mp4') ? MessageType.video : MessageType.image
+var mime = que[i].includes('mp4') ? Mimetype.mp4 : Mimetype.jpg
 var stream = get.buffer();
 stream.then(async (video) => {
-await msg.client.sendMessage(msg.jid, video, MessageType.video, { mimetype: Mimetype.mp4, caption: res.result.username, quoted: msg.data});
+await msg.sendMessage('```Downloading '+res.result.stories_count+' stories of '+res.result.username+'```');
+await msg.client.sendMessage(msg.jid, video, type, { mimetype: mime,quoted: msg.data});
 })};
 }));
