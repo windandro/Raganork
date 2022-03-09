b827f63ba0ed691434acf2390746a213f802567a
byadems <81581944+byadems@users.noreply.github.com>
2022-03-09 21:20:25 +0300

    Update warn.js

--- plugins/warn.js
+++ plugins/warn.js
@@ -16,31 +16,31 @@ var me = m.client.user.jid.split('@')[0]
 var chat = m.jid
 if (!chat.endsWith('@g.us')) return await m.sendMessage(Lang.WARNGROUP)
 var warn = await query.setwarn(me,chat,par,cnt,v)
-var reason = mat[1] ? mat[1] : 'Replied message'
-var msg = "```Warning ⚠️```"+ '\n' +
-"User: " +'@'+par.split('@')[0] + '\n' +
-"Reason:" +' *' + reason+ '*\n' +
-"Remaining:" +' *' + warn + '*\n' 
+var reason = mat[1] ? mat[1] : 'Sebepsiz'
+var msg = "```⚠️UYARI!```"+ '\n' +
+"Kullanıcı: " +'@'+par.split('@')[0] + '\n' +
+"Sebep:" +' *' + reason+ '*\n' +
+"Uyarı sayısı:" +' *' + warn + '*\n' 
 if (warn !== 0) {
     return await m.client.sendMessage(chat,msg,MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: [par]}})
 } else {
-    await m.client.sendMessage(chat,'⛔ @'+par.split('@')[0]+' *kullanıcısının uyarılma limiti '+cnt+'/3 ulaştığından dolayı gruptan çıkarıldı!*',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: [par]}})
+    await m.client.sendMessage(chat,'⛔ @'+par.split('@')[0]+' *adlı kullanıcının uyarılma limiti '+cnt+'/3 ulaştığından dolayı gruptan çıkarıldı!*',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: [par]}})
     await m.client.groupRemove(m.jid, [m.reply_message.data.participant]);
 }
 }));
 
 e.addCommand({pattern: 'resetwarn', fromMe: true, desc: Lang.RESETWARN_DESC}, (async (m, mat) => { 
-    if (!m.reply_message) return await m.sendMessage('_Reply to any message!_')
+    if (!m.reply_message) return await m.sendMessage(Lang.WARN_NEED)
         var par = m.reply_message.jid
     var me = m.client.user.jid.split('@')[0]
     var chat = m.jid
     if (!chat.endsWith('@g.us')) return await m.sendMessage(Lang.WARNGROUP)
     await query.deletewarn(me,chat,par,v)
-    await m.client.sendMessage(chat,'```Successfully reset warn limits ('+cnt+') of @'+par.split('@')[0]+ '```',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: [par]}})    
+    await m.client.sendMessage(chat,'✅ @'+par.split('@')[0]+' *adlı kullanıcının toplamda '+cnt+' uyarısı başarıyla silindi.*',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: [par]}})    
 }));
 
 e.addCommand({pattern: 'getwarn', fromMe: true, desc: Lang.GETWARN_DESC}, (async (m, mat) => { 
-    if (!m.reply_message) return await m.sendMessage('_Reply to any message!_')
+    if (!m.reply_message) return await m.sendMessage(Lang.WARN_NEED)
         var par = m.reply_message.jid
     var me = m.client.user.jid.split('@')[0]
     var chat = m.jid
@@ -48,10 +48,10 @@ e.addCommand({pattern: 'getwarn', fromMe: true, desc: Lang.GETWARN_DESC}, (async
     var war = await query.getwarn(me,chat,par,v)
     var warns = war.length
     if (warns === 0) {
-    return await m.client.sendMessage(chat,'```User @'+par.split('@')[0]+ ' is not in the warn list ✅```',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: [par]}})    
+    return await m.client.sendMessage(chat,'🥳 @'+par.split('@')[0]+ ' *kullanıcısına ait hiçbir uyarıya rastlayamadım.*',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: [par]}})    
     } else {
     var total = parseInt(cnt) - warns
-return await m.client.sendMessage(chat,'```User @'+par.split('@')[0]+' has only ('+total+') warnings left ⚠```',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: [par]}})    
+return await m.client.sendMessage(chat,'*⚠ @'+par.split('@')[0]+' adlı kullanıcının sadece '+total+' uyarısı kaldı!',MessageType.text,{quoted:m.data,contextInfo: {mentionedJid: [par]}})    
     }
 }));
     
