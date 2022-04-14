d477ee3d62c8ce1239b9adad2d7b51108fa6430a
byadems <81581944+byadems@users.noreply.github.com>
2022-04-14 17:33:56 +0300

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -9,8 +9,8 @@ const FormData = require('form-data')
 const Axios = require('axios');
 const Language = require('../language');
 const Lang = Language.getString('conventer');
-let sourav = Config.WORKTYPE == 'public' ? false : true
-    Asena.addCommand({pattern: 'photo$', fromMe: sourav, desc: Lang.STİCKER_DESC}, (async (message, match) => {   
+let sk = Config.WORKTYPE == 'public' ? false : true
+    Asena.addCommand({pattern: 'photo$', fromMe: sk, desc: Lang.STİCKER_DESC}, (async (message, match) => {   
         const mid = message.jid
         if (message.reply_message === false) return await message.client.sendMessage(mid, Lang.STİCKER_NEEDREPLY, MessageType.text);
         var downloading = await message.client.sendMessage(mid,Lang.STİCKER,MessageType.text);
@@ -32,7 +32,7 @@ let sourav = Config.WORKTYPE == 'public' ? false : true
     }));
 
 
-    Asena.addCommand({pattern: 'mp3$', fromMe: sourav, desc: 'Converts video/voice message to audio'}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'mp3$', fromMe: sk, desc: 'Converts video/voice message to audio'}, (async (message, match) => {    
        var rm = message.reply_message
        if (rm === false) return await message.client.sendMessage(message.jid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text,{quoted: message.data});
        if (!rm.audio && !rm.video) return await message.client.sendMessage(message.jid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text,{quoted: message.data});
