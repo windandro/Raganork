afd01809af36e799677397aef681ef49e5bb3929
byadems <81581944+byadems@users.noreply.github.com>
2022-04-14 17:42:23 +0300

    Update converter.js

--- plugins/converter.js
+++ plugins/converter.js
@@ -32,12 +32,19 @@ let sk = Config.WORKTYPE == 'public' ? false : true
     }));
 
 
-    Asena.addCommand({pattern: 'mp3$', fromMe: sk, desc: 'Converts video/voice message to audio'}, (async (message, match) => {    
+    Asena.addCommand({pattern: 'mp3$', fromMe: sk, desc: Lang.MP4TOAUDİO_DESC}, (async (message, match) => {    
        var rm = message.reply_message
        if (rm === false) return await message.client.sendMessage(message.jid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text,{quoted: message.data});
        if (!rm.audio && !rm.video) return await message.client.sendMessage(message.jid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text,{quoted: message.data});
        downloading = await message.client.sendMessage(message.jid,Lang.MP4TOAUDİO,MessageType.text,{quoted: message.data});
-       var location = await message.client.downloadAndSaveMediaMessage({key: {remoteJid: message.reply_message.jid,id: message.reply_message.id },message: message.reply_message.data.quotedMessage});
+       var location = await message.client.downloadAndSaveMediaMessage({
+           key: {
+               remoteJid: message.reply_message.jid,
+               id: message.reply_message.id 
+           },
+               message: message.reply_message.data.quotedMessage
+       });
+        
        ffmpeg(location)
             .save('tomp3.mp3')
             .on('end', async () => {
