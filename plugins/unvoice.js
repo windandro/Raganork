6268c981ba34b1fb72daa1c6ec9695891c985f1e
byadems <81581944+byadems@users.noreply.github.com>
2022-04-14 17:03:00 +0300

    Update unvoice.js

--- plugins/unvoice.js
+++ plugins/unvoice.js
@@ -18,16 +18,5 @@ Asena.addCommand({pattern: 'unvoice', fromMe: sourav, desc: Lang.UV_DESC}, (asyn
         .on('end', async () => {
             await message.sendMessage(fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true});});}));
 
-Asena.addCommand({pattern: 'mp3$', fromMe: sourav, desc: 'Converts video/voice message to audio'}, (async (message, match) => {    
-     var rm = message.reply_message
-     if (rm === false) return await message.client.sendMessage(message.jid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text,{quoted: message.data});
-    if (!rm.audio && !rm.video) return await message.client.sendMessage(message.jid, Lang.MP4TOAUDİO_NEEDREPLY, MessageType.text,{quoted: message.data});
-    downloading = await message.client.sendMessage(message.jid,Lang.MP4TOAUDİO,MessageType.text,{quoted: message.data});
-    var location = await message.client.downloadAndSaveMediaMessage({key: {remoteJid: message.reply_message.jid,id: message.reply_message.id },message: message.reply_message.data.quotedMessage});
-        ffmpeg(location)
-            .save('tomp3.mp3')
-            .on('end', async () => {
-                await message.client.sendMessage(message.jid, fs.readFileSync('tomp3.mp3'), MessageType.audio, {quoted:message.data,mimetype: Mimetype.mp4Audio, ptt: false});
-            });
-        }));
+
  
